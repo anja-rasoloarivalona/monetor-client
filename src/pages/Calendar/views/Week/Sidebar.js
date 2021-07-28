aedf3fa1d151aed5e7859da5801655e8f4d985db
anjarasoloarivalona <anja@bizbizshare.com>
2021-07-28 15:40:47 -0400

    Cleaned  calendar folder

--- src/pages/Calendar/views/Week/Sidebar.js
+++ src/pages/Calendar/views/Week/Sidebar.js
@@ -1,52 +1,75 @@
-import React, { useEffect } from "react"
+import React from "react"
 import styled from "styled-components"
 import { getHoursDate } from '../../functions'
 import { useSelector } from 'react-redux'
 
+
+const Empty = styled.div`
+    width: 100%;
+    grid-row: 1 / 2;
+    grid-column: 1 / 2;
+    position: sticky;
+    z-index: 2;
+    background: ${props => props.theme.surface};
+`
+
 const Container = styled.div`
-    width: 20rem;
+    width: 100%;
     border-right: 1px solid ${props => props.theme.form.unfocused.border};
+    grid-row: 2 / 3;
+    grid-column: 1 / 2;
+    position: relative;
+    z-index: 2;
+    background: ${props => props.theme.surface};
 `
 
 const List = styled.div`
     display: flex;
     flex-direction: column;
+
+    > div:last-child {
+        > div {
+            display: none;
+        }
+    }
 `
 
 const ListItem = styled.div`
     width: 100%;
-    height: 8rem;
     border-bottom: 1px solid ${props => props.theme.form.unfocused.border};
     display: flex;
-    align-items: start;
+    align-items: flex-end;
     justify-content: flex-end;
     font-size: 1.2rem;
-    padding: 1rem;
 `
 
+const ListItemLabel = styled.div`
+    padding: 1rem;
+    transform: translateY(50%);
+    background :white;
 
+`
 
-const Sidebar = props => {
-
+const Sidebar = () => {
     const {
         settings: { unitType }
     } = useSelector(state => state)
 
-    useEffect(() => {
-
-    },[])
-
-  
     return (
-        <Container className="sidebar">
-            <List>
-                {getHoursDate(unitType).map((h, index) => (
-                    <ListItem key={index} className="item">
-                        {h}
-                    </ListItem>
-                ))}
-            </List>
-        </Container>
+        <>
+            <Container className="sidebar">
+                <List className="sidebar__list">
+                    {getHoursDate(unitType).map((h, index) => (
+                        <ListItem key={index} className="sidebar__list__item">
+                            <ListItemLabel>
+                            {h}
+
+                            </ListItemLabel>
+                        </ListItem>
+                    ))}
+                </List>
+            </Container>
+        </>
      )
 };
 
